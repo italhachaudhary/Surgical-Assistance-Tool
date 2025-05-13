d8acd663b377c4b0ac0a59636c61c3674f9c04fa
M. Talha Bin Zulfiqar <2020-cs-5195@uosahiwal.edu.pk>
2025-05-13 14:45:04 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(page_title="Surgical Assistance Tool", layout="wide")
+st.title("🧠 Surgical Assistance Tool")
+
+tab1, tab2 = st.tabs(["📤 Upload Image", "📸 Live Detection"])
+
+with tab1:
+exec(open("image_detector_app.py").read())
+
+with tab2:
+exec(open("testWith_camera.py").read())
